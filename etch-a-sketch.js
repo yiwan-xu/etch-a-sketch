55099b2f73118e3b7f5628833d302ece62f6182e
yiwan-xu <292851390+yiwan-xu@users.noreply.github.com>
2026-07-30 20:54:12 +0100

    Add function to generate grid of any size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,26 @@
 //building 16 divs
 const container = document.querySelector(".container");
-n = 16;
-for (let i = 0; i < n; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    container.appendChild(box);
-}
 
+function makeGrid(size) {
+    container.style.width = `${20*size}px`;
+    for (let i = 0; i < size*size; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        container.appendChild(box);
+    }
+}
+makeGrid(8);
+
+//adding hover effect
+const boxes = document.querySelectorAll(".box")
+boxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "pink";
+        setTimeout(() => {
+            box.style.backgroundColor = "white";
+        }, 5000);
+    })
+})
+
+//function to reset board
+//function resetBoard()
