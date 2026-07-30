28c69f4f78e52bcf66d6f91874530a35a3d89da8
yiwan-xu <292851390+yiwan-xu@users.noreply.github.com>
2026-07-30 23:11:12 +0100

    Add reset board feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 
 function makeGrid(size) {
     container.style.width = `${20*size}px`;
+    container.style.height = `${20*size}px`;
     for (let i = 0; i < size*size; i++) {
         const box = document.createElement("div");
         box.classList.add("box");
@@ -12,15 +13,32 @@ function makeGrid(size) {
 makeGrid(8);
 
 //adding hover effect
-const boxes = document.querySelectorAll(".box")
-boxes.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = "pink";
-        setTimeout(() => {
-            box.style.backgroundColor = "white";
-        }, 5000);
+function newRound() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "pink";
+            //*disappearing boxes feature
+            //setTimeout(() => {
+            //    box.style.backgroundColor = "white";
+            //}, 5000);
+        })
     })
-})
+}
+newRound();
 
-//function to reset board
-//function resetBoard()
+//button to reset board
+const reset = document.querySelector("button");
+reset.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach(box => box.remove());
+    let answer = prompt("Enter desired board width (in number of boxes:)");
+    if (answer <= 100 && answer >= 1) {
+        makeGrid(answer);
+        newRound();
+    } else {
+        answer = prompt("Please re-enter an integer width between 1 and 100:");
+        makeGrid(answer);
+        newRound();
+    }
+})
